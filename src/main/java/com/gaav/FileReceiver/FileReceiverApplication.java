3af902f81d3daa1bdd1fe21dcba97f4adc5d1422
tecnologia-9 <tecnologia9@tecnologia9.local>
2022-10-19 09:51:28 -0500

    file upload implemented

--- src/main/java/com/gaav/FileReceiver/FileReceiverApplication.java
+++ src/main/java/com/gaav/FileReceiver/FileReceiverApplication.java
@@ -1,9 +1,14 @@
 package com.gaav.FileReceiver;
 
+import com.gaav.FileReceiver.properties.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class FileReceiverApplication {
 
 	public static void main(String[] args) {
